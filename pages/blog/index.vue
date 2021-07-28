eeb629bdb2866bd7276e72af8e01826f93c9aa1a
MiguelMolina29 <miguelantonio.molinago@my.uvm.edu.mx>
2021-07-27 21:59:50 -0500

    Primero

---
+++ pages/blog/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <ul>
+      <li>Articulo 1</li>
+      <li>Articulo 2</li>
+      <li>Articulo 3</li>
+      <li>Articulo 4</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
